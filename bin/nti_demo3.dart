4d301d2fd26fffcb0e6c41957827e0b64e39bd75
Pierre-Soliman <117318582+Pierre-Soliman@users.noreply.github.com>
2025-11-29 21:34:39 +0200

    Peter frist task

---
+++ bin/nti_demo3.dart
@@ -0,0 +1,3 @@
+import 'package:nti_demo3/nti_demo3.dart' as nti_demo3;
+
+void main(List<String> arguments) {}
---
+++ test/nti_demo3_test.dart
@@ -0,0 +1,8 @@
+import 'package:nti_demo3/nti_demo3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect( (),42);
+  });
+}
